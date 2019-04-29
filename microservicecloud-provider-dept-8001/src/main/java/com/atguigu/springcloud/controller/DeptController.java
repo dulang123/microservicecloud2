ee9a24b517c521626542153b67527a788f0a3fde
dulang123 <1282032739@qq.com>
2019-04-29 16:37:25 +0800

    test

--- microservicecloud-provider-dept-8001/src/main/java/com/atguigu/springcloud/controller/DeptController.java
+++ microservicecloud-provider-dept-8001/src/main/java/com/atguigu/springcloud/controller/DeptController.java
@@ -1,23 +1,20 @@
 package com.atguigu.springcloud.controller;
 
-import java.util.List;
-
+import com.atguigu.springcloud.entities.Dept;
+import com.atguigu.springcloud.service.DeptService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import com.atguigu.springcloud.entities.Dept;
-import com.atguigu.springcloud.service.DeptService;
+import java.util.List;
 
 @RestController
 public class DeptController
 {
-	@Autowired
+
+    @Autowired
 	private DeptService service;
 	@Autowired
 	private DiscoveryClient client;
@@ -29,16 +26,34 @@ public class DeptController
 	}
 
 	@RequestMapping(value = "/dept/get/{id}", method = RequestMethod.GET)
+	//一旦调用服务方法失败并抛出了错误信息后，会自动调用@HystrixCommand标注好的fallbackMethod调用类中的指定方法
+	@HystrixCommand(fallbackMethod = "processHystrix_Get")
 	public Dept get(@PathVariable("id") Long id)
 	{
-		return service.get(id);
+
+		Dept dept = this.service.get(id);
+
+		if (null == dept) {
+			throw new RuntimeException("该ID：" + id + "没有没有对应的信息");
+		}
+
+		return dept;
 	}
 
-	@RequestMapping(value = "/dept/list", method = RequestMethod.GET)
-	public List<Dept> list()
+	public Dept processHystrix_Get(@PathVariable("id") Long id)
 	{
-		return service.list();
+
+		System.out.print("*********该ID没有没有对应的信息*8*******");
+		Dept dept=new Dept("该ID：" + id + "没有没有对应的信息,Consumer客户端提供的降级信息,此刻服务Provider已经关闭");
+		dept.setDeptno(id);
+		dept.setDb_source("no this database in MySQL");
+		dept.setDname("该ID：" + id + "没有没有对应的信息,Consumer客户端提供的降级信息,此刻服务Provider已经关闭");
+		return dept;
 	}
+	@RequestMapping(value = "/dept/list", method = RequestMethod.GET)
+	public List<Dept> list() { return service.list(); }
+
+
 
 	
 //	@Autowired
@@ -56,5 +71,7 @@ public class DeptController
 		}
 		return this.client;
 	}
+
+
 
 }
